81c7614d719a24f3c11e688625c4a20f999c2946
J-Double-J <joshua.jacobs126@gmail.com>
2024-12-16 12:46:26 -0600

    Grid pathfinding algorithms can now consider a grid cell's navigability

--- Runtime/Systems/Grid/Pathfinding/AStarGrid.cs
+++ Runtime/Systems/Grid/Pathfinding/AStarGrid.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Venwin.DataStructures;
 
 #nullable enable
@@ -13,9 +14,16 @@ namespace Venwin.Grid.Pathfinding
         /// </summary>
         /// <param name="start">Starting cell.</param>
         /// <param name="goal">End cell.</param>
+        /// <param name="considerIsNavigatable">If set to true cells are not considered part of the pathfinding unless <see cref="GridCell.IsNavigatable"/> is set to true on a Grid Cell's neigbors.</param>
         /// <returns>The path of <see cref="GridCell"/>'s pointing to the previous cell it came from. If no path is found returns null.</returns>
-        public static Dictionary<GridCell, GridCell>? FindPath(GridCell start, GridCell goal)
+        /// <exception cref="ArgumentException">Thrown if <paramref name="considerIsNavigatable"/> is true and the <paramref name="start"/> or <paramref name="goal"/> cells have <see cref="GridCell.IsNavigatable"/> set to false. </exception>
+        public static Dictionary<GridCell, GridCell>? FindPath(GridCell start, GridCell goal, bool considerIsNavigatable = false)
         {
+            if(considerIsNavigatable)
+            {
+                if(!start.IsNavigatable) { throw new ArgumentException("Start Cell must be navigatable"); }
+                if (!goal.IsNavigatable) { throw new ArgumentException("End Cell must be navigatable"); }
+            }
             var frontier = new PriorityQueue<GridCell>();
             frontier.Enqueue(start, 0);
 
@@ -34,7 +42,10 @@ namespace Venwin.Grid.Pathfinding
                     return cameFrom;
                 }
 
-                foreach (var next in current.Neighbors)
+                IReadOnlyCollection<GridCell> nextCellsToConsider = considerIsNavigatable ? current.Neighbors.Where(cell => cell.IsNavigatable).ToList()
+                                                                                          : current.Neighbors;
+
+                foreach (var next in nextCellsToConsider)
                 {
                     int newCost = costSoFar[current] + next.GetCostToEnter(current);
                     if (!costSoFar.ContainsKey(next) || newCost < costSoFar[next])
@@ -55,10 +66,12 @@ namespace Venwin.Grid.Pathfinding
         /// </summary>
         /// <param name="start">Starting cell.</param>
         /// <param name="goal">Ending cell.</param>
+        /// <param name="considerIsNavigatable">If set to true cells are not considered part of the pathfinding unless <see cref="GridCell.IsNavigatable"/> is set to true on a Grid Cell's neigbors.</param>
         /// <returns>A list of the cells from start to end. If none is found, returns null instead.</returns>
-        public static List<GridCell>? GetPath(GridCell start, GridCell goal)
+        /// <exception cref="ArgumentException">Thrown if <paramref name="considerIsNavigatable"/> is true and the <paramref name="start"/> or <paramref name="goal"/> cells have <see cref="GridCell.IsNavigatable"/> set to false. </exception>
+        public static List<GridCell>? GetPath(GridCell start, GridCell goal, bool considerIsNavigatable = false)
         {
-            return GetPathFromFoundPath(FindPath(start, goal), start, goal);
+            return GetPathFromFoundPath(FindPath(start, goal, considerIsNavigatable), start, goal);
         }
 
         private static List<GridCell>? GetPathFromFoundPath(Dictionary<GridCell, GridCell>? cameFrom, GridCell start, GridCell goal)
